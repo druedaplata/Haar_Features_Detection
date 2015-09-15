9d23eefa7be824a50915933f17ecb31c59336ece
Diego Rueda <sandiego206@gmail.com>
2015-09-14 19:11:55 -0500

    add samples

--- simple_hand/video.py
+++ simple_hand/video.py
@@ -1,10 +1,11 @@
 import cv2
 import matplotlib.pyplot as plt
 
-def video_reception(classifier1, scale_factor=1.1, min_neighbors=5, min_size=(64,64)):
+def video_reception(classifier1, classifier2, scale_factor=1.1, min_neighbors=1, min_size=(64,64)):
     
     # Cargar el clasificador
     cascade1 = cv2.CascadeClassifier(classifier1)
+    cascade2 = cv2.CascadeClassifier(classifier2)    
 
     # Recibir video de la entrada
     video_capture = cv2.VideoCapture(0)
@@ -26,11 +27,25 @@ def video_reception(classifier1, scale_factor=1.1, min_neighbors=5, min_size=(64
             flags=cv2.cv.CV_HAAR_SCALE_IMAGE
         )
 
+	# Detectar fist en la entrada de video
+	features2 = cascade2.detectMultiScale(
+	    gray,
+	    scaleFactor=scale_factor,
+            minNeighbors=min_neighbors,
+            minSize=min_size,
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+ 
         # Iterar sobre cada imagen encontrada 
         for (x, y, w, h) in features1:
             # Descomentar la siguiente linea para debug (dibuja una caja en todas las caras )
             feature = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
         
+	# Iterar sobre cada imagen encontrada 
+        for (x, y, w, h) in features2:
+            # Descomentar la siguiente linea para debug (dibuja una caja en todas las caras )
+            feature = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
         # Mostrar la imagen resultante
         cv2.imshow('Video', frame)
  
@@ -43,4 +58,4 @@ def video_reception(classifier1, scale_factor=1.1, min_neighbors=5, min_size=(64
     video_capture.release()
     cv2.destroyAllWindows()
     
-video_reception("final.xml")
+video_reception("simple_palm.xml", "simple_fist.xml")
